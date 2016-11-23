6d9e522f3700a6871a0b20fa1b0de8160e93f105
Bushra-awwad <bushra.s.awwad@gmail.com>
2016-11-23 21:59:51 +0300

    changes

---
+++ app/src/main/java/mbeeseals/com/mbeedeals/signUp.java
@@ -0,0 +1,32 @@
+package mbeeseals.com.mbeedeals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.SignInButton;
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        SignInButton Signup = (SignInButton) findViewById(R.id.google);
+        setGooglePlusButtonText(Signup , getString(R.string.google));
+
+            }
+    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View v = signInButton.getChildAt(i);
+
+            if (v instanceof TextView) {
+                TextView tv = (TextView) v;
+                tv.setText(buttonText);
+                return;
+            }
+        }
+    }
+}
