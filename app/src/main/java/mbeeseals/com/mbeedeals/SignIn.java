32162022c4590772b00480a0b5e2906db2200e98
Bushra-awwad <bushra.s.awwad@gmail.com>
2016-11-25 02:59:47 +0300

    changes

--- app/src/main/java/mbeeseals/com/mbeedeals/SignIn.java
+++ app/src/main/java/mbeeseals/com/mbeedeals/SignIn.java
@@ -5,25 +5,26 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+
 import com.google.android.gms.common.SignInButton;
 
-public class signUp extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
 
         Toolbar title_bar  = (Toolbar) findViewById(R.id.title_bar);
         setSupportActionBar(title_bar);
 
         TextView title = (TextView) findViewById(R.id.mytext) ;
-        title.setText(R.string.Register);
+        title.setText(R.string.Sign_in);
 
         SignInButton Signup = (SignInButton) findViewById(R.id.google);
-        setGooglePlusButtonText(Signup , getString(R.string.google));
+        setGooglePlusButtonText(Signup , getString(R.string.google_sign_in));
+    }
 
-            }
     protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
         // Find the TextView that is inside of the SignInButton and set its text
         for (int i = 0; i < signInButton.getChildCount(); i++) {
@@ -35,5 +36,4 @@ public class signUp extends AppCompatActivity {
                 return;
             }
         }
-    }
-}
+    }}
--- app/src/main/java/mbeeseals/com/mbeedeals/introActivity.java
+++ app/src/main/java/mbeeseals/com/mbeedeals/introActivity.java
@@ -2,7 +2,11 @@ package mbeeseals.com.mbeedeals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class introActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class introActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Toolbar title_bar  = (Toolbar) findViewById(R.id.title_bar);
+        setSupportActionBar(title_bar);
+
+        TextView title = (TextView) findViewById(R.id.mytext) ;
+        title.setText(R.string.app_name);
+
+        ImageButton arrow = (ImageButton) findViewById(R.id.arrowback) ;
+          arrow.setVisibility(View.GONE);
     }
 }
